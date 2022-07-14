0ff2813571db6bb77017f47cca27e8f6dfa1b495
Jakub Kapitulcin <jakkappro@gmail.com>
2022-07-14 10:45:00 +0200

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+var baseAddress = new Uri("https://private-anon-935b1c06cc-expandopublicapi.apiary-mock.com/api/v2/");
+var access_token = "11w1QgSM7YR4tHyr4BR0BV";
+var days = 1;
+
+
+using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
+{
+
+  using(var response = await httpClient.GetAsync("orderfeed?access_token=" + access_token + "&days=" + days))
+  {
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+  }
+}
+
