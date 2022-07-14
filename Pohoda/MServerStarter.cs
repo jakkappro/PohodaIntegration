ff1c2d9a3a0d7fc63a03a703fe1433a9d1a98cd6
Jakub Kapitulcin <jakkappro@gmail.com>
2022-07-14 18:09:41 +0200

    refactor starting of mServer

---
+++ Pohoda/MServerStarter.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Xml;
+
+namespace PohodaIntegration.Pohoda
+{
+    internal class MServerStarter
+    {
+        public MServerStarter()
+        {
+
+        }
+
+        public async Task<bool> startAsync(string serverName, string pathToServer, string serverUrl, string username, string password, short retryDelay, short amountOfRetries)
+        {
+            var mServerStartComand = $"cd \"{pathToServer}\" & pohoda.exe /http start {serverName}";
+
+            var cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = false;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(mServerStartComand);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+
+            return await testConnectionAsync(serverUrl, username, password, retryDelay, amountOfRetries, serverName);
+        }
+
+        private async Task<bool> testConnectionAsync(string serverUrl, string username, string password, short retryDelay, short amountOfmServerRetries, string serverName)
+        {
+            using (var httpClient = new HttpClient { BaseAddress = new Uri(serverUrl) })
+            {
+                httpClient.DefaultRequestHeaders.Add("STW-Authorization", createAuthHeader(username, password));
+                httpClient.DefaultRequestHeaders.Add("Accept", "text/xml");
+
+                var responseCode = HttpStatusCode.BadRequest;
+
+                while (responseCode != HttpStatusCode.OK && amountOfmServerRetries >= 0)
+                {
+                    amountOfmServerRetries--;
+
+                    try
+                    {
+                        using (var response = await httpClient.GetAsync("/status"))
+                        {
+                            responseCode = response.StatusCode;
+                            if (response.StatusCode != HttpStatusCode.OK)
+                            {
+                                Console.WriteLine("Error: Couldn't connect to mServer \nResponseCode" + response.StatusCode);
+                                continue;   
+                            }
+
+                            var xmlDoc = new XmlDocument();
+                            var responseString = await response.Content.ReadAsStringAsync();
+                            xmlDoc.LoadXml(responseString);
+                            try
+                            {
+                                var actualServerName = xmlDoc.GetElementsByTagName("name")[0].InnerText;
+
+                                if (!actualServerName.Equals(serverName))
+                                {
+                                    return false;
+                                }
+
+                                Console.WriteLine("mServer is running on " + serverName);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Error: Couldn't get mServer name");
+                                return false;
+                            }
+                        }
+                    }
+                    catch (System.Net.Http.HttpRequestException)
+                    {
+                    }
+
+                    await Task.Delay(retryDelay);
+                }
+
+                Console.WriteLine("Couldn't connect to the server");
+
+                return false;
+            }
+        }
+
+        private string createAuthHeader(string username, string password)
+        {
+            return "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{username}:{password}"));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Diagnostics;
-using System.Net.Http;
-using System.Net;
+﻿using System.Net;
 using System.Xml;
 using Microsoft.Extensions.Configuration;
+using PohodaIntegration.Pohoda;
 
 var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
@@ -14,87 +12,41 @@ var baseAddress = new Uri(configuration.GetSection("Expando:mockServerBaseAdress
 var access_token = configuration.GetSection("Expando:apiKey").Value;
 var days = 1;
 
-var mServerAccess = $"{configuration.GetSection("Pohoda:mServer:username")}:{configuration.GetSection("Pohoda:mServer:password")}";
+string username = configuration.GetSection("Pohoda:mServer:username").Value ?? throw new ArgumentException("Missing Pohoda username.");
+string password = configuration.GetSection("Pohoda:mServer:password").Value ?? throw new ArgumentException("Missing Pohoda password.");
+
+if (username == null || password == null)
+{
+    Console.WriteLine("Missing username or password in pohoda configuration");
+    return;
+}
+
 var pohodaPort = 5336;
 var pohodaHost = "http://127.0.0.1:" + pohodaPort;
 var pohodamServerName = "test";
 var pathToPohodamServer = "\"C:\\Program Files (x86)\\STORMWARE\\POHODA SK E1\"";
 var mServerStartCommand = $"cd {pathToPohodamServer} && pohoda.exe /http start {pohodamServerName}";
 Console.WriteLine(mServerStartCommand);
-var authHeader = "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{mServerAccess}:"));
-var mServerBaseAddress = new Uri(pohodaHost);
-var amountOfmServerRetries = 3;
-var delayBetweenRetries = 1000;
+short amountOfmServerRetries = 3;
+short delayBetweenRetries = 1000;
 
 // start mServer
-var cmd = new Process();
-cmd.StartInfo.FileName = "cmd.exe";
-cmd.StartInfo.RedirectStandardInput = true;
-cmd.StartInfo.RedirectStandardOutput = false;
-cmd.StartInfo.CreateNoWindow = true;
-cmd.StartInfo.UseShellExecute = false;
-cmd.Start();
-
-cmd.StandardInput.WriteLine(mServerStartCommand);
-cmd.StandardInput.Flush();
-cmd.StandardInput.Close();
-cmd.WaitForExit();
+var mServer = new MServerStarter();
+await mServer.startAsync("test", pathToPohodamServer, pohodaHost, username, password, delayBetweenRetries, amountOfmServerRetries);
 
 // test if mServer is running
-using (var httpClient = new HttpClient{ BaseAddress = mServerBaseAddress }) 
-{
-  httpClient.DefaultRequestHeaders.Add("STW-Authorization", authHeader);
-  httpClient.DefaultRequestHeaders.Add("Accept", "text/xml");
-  
-  var responseCode = HttpStatusCode.BadRequest;
 
-  while (responseCode != HttpStatusCode.OK && amountOfmServerRetries >= 0)
-  {
-    amountOfmServerRetries--;
-
-    try 
-    {
-      using (var response = await httpClient.GetAsync("/status")) 
-        {
-          responseCode = response.StatusCode;
-          if (response.StatusCode != HttpStatusCode.OK) {
-            Console.WriteLine("Error: Couldn't connect to mServer \nResponseCode" + response.StatusCode);
-            return;
-          }
-
-          var xmlDoc = new XmlDocument();
-          var responseString = await response.Content.ReadAsStringAsync();
-          xmlDoc.LoadXml(responseString);
-          try 
-          {
-          var serverName = xmlDoc.GetElementsByTagName("name")[0].InnerText;
-          Console.WriteLine("mServer is running on " + serverName);
-          }
-          catch (Exception) 
-          {
-            Console.WriteLine("Error: Couldn't get mServer name");
-            return;
-          }
-      }
-    } 
-    catch (System.Net.Http.HttpRequestException) 
-    {
-    }
-
-    await Task.Delay(delayBetweenRetries);
-  }  
-}
 
 return;
 
 // get orders from mServer
 
 // get orders from Expando
-using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
-{
-  using(var response = await httpClient.GetAsync("orderfeed?access_token=" + access_token + "&days=" + days))
-  {
-    var content = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(content);
-  }
-}
+//using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+//{
+//    using (var response = await httpClient.GetAsync("orderfeed?access_token=" + access_token + "&days=" + days))
+//    {
+//        var content = await response.Content.ReadAsStringAsync();
+//        Console.WriteLine(content);
+//    }
+//}
